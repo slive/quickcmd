ebf368a7d9df9c2313708bbe0145f0825100837a
unknown <liangfengxx87@gail.com>
2014-07-31 23:40:22 +0800

    .

---
+++ org.slive.quickcmd/src/org/slive/quickcmd/actions/QuickCmdAction.java
@@ -0,0 +1,119 @@
+package org.slive.quickcmd.actions;
+
+import java.io.File;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jdt.internal.core.JarPackageFragmentRoot;
+import org.eclipse.jdt.internal.core.PackageFragment;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+/**
+ * 
+ * @author Slive
+ *
+ */
+@SuppressWarnings("restriction")
+public class QuickCmdAction implements IObjectActionDelegate 
+{
+	private Object selected = null;
+	private Class<?> selectedClass = null;
+
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) 
+	{
+	}
+
+	public void run(IAction action) 
+	{
+		if (this.selected == null) 
+		{
+			MessageDialog.openInformation(new Shell(), "Quick Cmd",
+					"Unable to cmd " + this.selectedClass.getName());
+			return;
+		}
+		File directory = null;
+		if ((this.selected instanceof IResource)) 
+		{
+			directory = new File(((IResource) this.selected).getLocation()
+					.toOSString());
+		} 
+		else if ((this.selected instanceof File)) {
+			directory = (File) this.selected;
+		}
+
+		try 
+		{
+			String filePath = "";
+			String startPath = "c:";
+			if (directory != null) 
+			{
+				if (directory.isDirectory()) 
+				{
+					filePath = directory.getAbsolutePath();
+				} 
+				else 
+				{
+					filePath = directory.getParentFile().getAbsolutePath();
+				}
+				
+				// 截取当前文件的开头盘符
+				startPath = filePath.substring(0, 2);
+			}
+			
+			//运行CMD命令，并切换到当前目录下
+			Runtime.getRuntime().exec("cmd /c start cd " + filePath, null,
+					new File(startPath));
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) 
+	{
+		IAdaptable adaptable = null;
+		this.selected = null;
+		if ((selection instanceof IStructuredSelection)) 
+		{
+			adaptable = (IAdaptable) ((IStructuredSelection) selection).getFirstElement();
+			this.selectedClass = adaptable.getClass();
+			if ((adaptable instanceof IResource))
+			{
+				this.selected = ((IResource) adaptable);
+			}
+			else if (((adaptable instanceof PackageFragment))
+					&& ((((PackageFragment) adaptable).getPackageFragmentRoot() instanceof JarPackageFragmentRoot)))
+			{
+				this.selected = getJarFile(((PackageFragment) adaptable)
+						.getPackageFragmentRoot());
+			}
+			else if ((adaptable instanceof JarPackageFragmentRoot))
+			{
+				this.selected = getJarFile(adaptable);
+			}
+			else
+			{
+				this.selected = ((IResource) adaptable.getAdapter(IResource.class));
+			}
+		}
+	}
+
+	protected File getJarFile(IAdaptable adaptable) 
+	{
+		JarPackageFragmentRoot jpfr = (JarPackageFragmentRoot) adaptable;
+		File selected = jpfr.getPath().makeAbsolute().toFile();
+		if (!selected.exists()) 
+		{
+			File projectFile = new File(jpfr.getJavaProject().getProject()
+					.getLocation().toOSString());
+			selected = new File(projectFile.getParent() + selected.toString());
+		}
+		return selected;
+	}
+}
